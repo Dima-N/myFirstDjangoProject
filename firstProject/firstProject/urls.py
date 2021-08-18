054ce6603d6f27fc09942c5ce3d7adb3d71f19f3
diman <dima4nik@gmail.com>
2021-08-18 21:13:53 +0300

    work by loop with list in template

--- firstProject/firstProject/urls.py
+++ firstProject/firstProject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import ruletka.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ruletka.views.show_student),
 ]
